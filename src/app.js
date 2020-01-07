b090fb9a7fca7aed5feeafc8c7acd3c67e4d787c
Singlesaul <singlesaulwork@gmail.com>
2020-01-07 17:35:29 +0800

    配置 parcel，将 button 改写为单文件组件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import  Button from './components/button'
+
+Vue.component('g-button',Button)
+
+new Vue({
+    el:'#app'
+})
---
+++ src/components/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name:'buttonOne'
+    }
+</script>
+
+<style lang="scss">
+    .g-button {
+        padding: 0 1em;
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background: var(--button-background);
+        border-color: var(--border-color);
+        color: var(--color);
+        border-radius: var(--border-radius);
+        &:hover{
+            border-color: var(--border-color-hover);
+            cursor: pointer;
+        }
+        &:active{
+            background: var(--button-active-background);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+</style>
