e0622b814de9dc14cb12df2558e6caf5688e521f
eden <singlesaul@163.com>
2020-01-08 00:29:06 +0800

    添加icon.vue组件，优化生成

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import  Button from './components/button'
+import Button from './components/button'
+import Icon from './components/icon'
 
 Vue.component('g-button',Button)
+Vue.component('g-icon',Icon)
 
 new Vue({
     el:'#app'
