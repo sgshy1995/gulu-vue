130240894ae5cd47ea1a1df857c466a3404cb39c
EdenSheng <singlesaul@163.com>
2020-01-09 17:11:11 +0800

    删除注释

--- src/app.js
+++ src/app.js
@@ -106,13 +106,12 @@ chai.use(spies)
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
         propsData: {
-            icon: 'settings',
+            icon: 'download'
         }
     })
     vm.$mount()
     let spy = chai.spy(()=>{})
     vm.$on('update:loading', spy)
-    // 希望这个函数被执行
     let button = vm.$el
     button.click()
     expect(spy).to.have.been.called()
