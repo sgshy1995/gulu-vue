2e53807e4fee44ba27c8c20b06ea7b28e66400f6
EdenSheng <singlesaul@163.com>
2020-01-09 11:40:13 +0800

    使用 chai.expect 添加测试用例

--- src/app.js
+++ src/app.js
@@ -20,3 +20,80 @@ new Vue({
 
 // 单元测试
 
+import chai from 'chai'
+const expect = chai.expect
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'download'
+        }
+    })
+    vm.$mount()
+    let use = vm.$el.querySelector('use')
+    expect(use.getAttribute('xlink:href')).to.eq('#idownload')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'praise',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let use = vm.$el.querySelector('use')
+    expect(use.getAttribute('xlink:href')).to.eq('#iloading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'praise'
+        }
+    })
+    vm.$mount('#app1')
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'praise',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount('#app2')
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'download'
+        }
+    })
+    vm.$mount()
+    vm.$on('click', function () {
+        console.log(1)
+    })
+    let button = vm.$el
+    button.click()
+}
