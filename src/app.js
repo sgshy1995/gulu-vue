983aab2bf076d97b117f4a6c521efa651e18932f
EdenSheng <singlesaul@163.com>
2020-01-15 11:30:48 +0800

    实现 autoclose closeButton enableHtml 优化CSS样式

--- src/app.js
+++ src/app.js
@@ -19,9 +19,9 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
-Vue.component('g-layout',Layout)
-Vue.component('g-header',Header)
-Vue.component('g-sider',Sider)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-sider', Sider)
 Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-toast', Toast)
@@ -37,117 +37,18 @@ new Vue({
             message: '支持双向绑定'
         }
     },
+    created() {
+    },
     methods: {
         showToast() {
-            this.$toast('Hello World')
+            this.$toast('Hello World', {
+                closeButton: {
+                    text: '关闭',
+                    callback() {
+                        console.log('知道了')
+                    }
+                }
+            })
         }
     }
 })
-
-/* // 单元测试
-
-import chai from 'chai'
-import spies from 'chai-spies'
-const expect = chai.expect
-chai.use(spies)
-
-try {
-    {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData:{
-                icon: 'download'
-            }
-        })
-        vm.$mount()
-        let use = vm.$el.querySelector('use')
-        expect(use.getAttribute('xlink:href')).to.eq('#idownload')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-
-    {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData: {
-                icon: 'praise',
-                loading: true
-            }
-        })
-        vm.$mount()
-        let use = vm.$el.querySelector('use')
-        expect(use.getAttribute('xlink:href')).to.eq('#iloading')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-
-    {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData: {
-                icon: 'praise'
-            }
-        })
-        vm.$mount('#app1')
-        let svg = vm.$el.querySelector('svg')
-        let { order } = window.getComputedStyle(svg)
-        expect(order).to.eq('1')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-
-    {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData: {
-                icon: 'praise',
-                iconPosition: 'right'
-            }
-        })
-        vm.$mount('#app2')
-        let svg = vm.$el.querySelector('svg')
-        let { order } = window.getComputedStyle(svg)
-        expect(order).to.eq('2')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-
-
-    {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData: {
-                icon: 'download'
-            }
-        })
-        vm.$mount()
-        let spy = chai.spy(function () {})
-        vm.$on('click', spy)
-        let button = vm.$el
-        console.log(button)
-        button.click()
-        expect(spy).to.have.been.called()
-    }
-
-    {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData: {
-                icon: 'download'
-            }
-        })
-        vm.$mount()
-        let spy = chai.spy(()=>{})
-        vm.$on('update:loading', spy)
-        let button = vm.$el
-        button.click()
-        expect(spy).to.have.been.called()
-    }
-
-}catch (error) {
-    window.errors = [error]
-} finally {
-    window.errors && window.errors.map((error) => {
-        console.error(error.message)
-    })
-} */
--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,31 +1,102 @@
 <template>
-    <div class="toast">
-        <slot></slot>
+  <div class="toast" ref="toast">
+    <div class="message">
+      <slot v-if="!enableHtml"></slot>
+      <div v-else v-html="$slots.default"></div>
     </div>
+    <div class="line" ref="line"></div>
+    <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
 export default {
-    name:'GToast'
-}
+  name: "GToast",
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 3
+    },
+    closeButton: {
+      type: Object,
+      default() {
+        return {
+          text: "关闭",
+          callback: () => {}
+        };
+      }
+    },
+    enableHtml: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted() {
+    this.setStyles();
+    this.executeAutoClose();
+  },
+  methods: {
+    setStyles() {
+      this.$nextTick(() => {
+        this.$refs.line.style.height = `${
+          this.$refs.toast.getBoundingClientRect().height
+        }px`;
+      });
+    },
+    executeAutoClose() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoCloseDelay * 1000);
+      }
+    },
+    close() {
+      this.$el.remove();
+      this.$destroy();
+    },
+    onClickClose() {
+      this.close();
+      if (this.closeButton && typeof this.closeButton.callback === "function") {
+        this.closeButton.callback();
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.toast{
-    position: fixed;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    background: rgba(0,0,0,0.75);
-    color: white;
-    padding: 0 .4em;
-    border-radius: 4px;
-    font-size: 14px;
-    line-height: 1.8;
-    height: 30px;
-    display: flex;
-    align-items: center;
-    box-shadow: 0 0 3px 0 rgba(0,0,0,0.50);
+.toast {
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  background: rgba(0, 0, 0, 0.75);
+  color: white;
+  padding: 0 0.8em;
+  border-radius: 4px;
+  font-size: 14px;
+  line-height: 1.8;
+  min-height: 30px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  text-align: center;
+  .message{
+      padding: 0.3em 0;
+  }
+  .line {
+    border-left: 1px solid #666;
+    height: 100%;
+    margin-left: 0.8em;
+  }
+  .close {
+    padding-left: 0.8em;
+    cursor: pointer;
+    flex-shrink: 0;
+  }
 }
 </style>
